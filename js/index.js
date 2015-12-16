196591b3dac33ff55fd4af297abd861d54799c34
Celso Mireles <celso@digitalstrategy.tips>
2015-12-15 20:00:01 -0700

    Initial Commit

---
+++ js/index.js
@@ -0,0 +1,18 @@
+// $(document).ready(function() {
+//     var activeClass = 'active';
+//     $('.map-point').click(function(event) {
+//         event.preventDefault();
+//         event.stopPropagation();
+//         $('.map-point').removeClass(activeClass);
+//         $(event.target).addClass(activeClass);
+//
+//
+//     });
+//
+//
+//
+//     $('html').click(function() {
+//         $('.map-point').removeClass(activeClass);
+//         event.stopPropagation();
+//     });
+// });
