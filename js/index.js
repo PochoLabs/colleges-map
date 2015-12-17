691a4ade14a1ce71d17359be870a478ad89fac34
Celso Mireles <celso@digitalstrategy.tips>
2015-12-17 15:04:41 -0700

    Update CSS
    Now works on Safari and Chrome, but not Firefox

--- js/index.js
+++ js/index.js
@@ -1,21 +1,24 @@
 $(document).ready(function(){
   var activeClass = 'active';
 
-  $('.centered-y > p > a, a').click(function(e){
-    e.preventDefault();
-    var url = $(this).attr('href');
-    console.log(url + ' clicked');
-    window.open(url, '_blank');
-  });
+  // $('.centered-y > p > a').click(function(e){
+  //   e.preventDefault();
+  //   var url = $(this).attr('href');
+  //   window.open(url, '_blank');
+  // });
 
   $('.map-point').click(function(e){
     $('.map-point').removeClass(activeClass);
     $(this).addClass(activeClass);
     e.stopPropagation();
+		var useThis = $(this);
+		
+
   });
 
   $('body').click(function(e){
     $('.map-point').removeClass(activeClass);
     e.stopPropagation();
+
   });
 });
