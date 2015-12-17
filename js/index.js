878bffce049891f262f1c3643eb3ff54263ab894
Celso Mireles <celso@digitalstrategy.tips>
2015-12-17 16:52:19 -0700

    Getting ready to take out inline styles

--- js/index.js
+++ js/index.js
@@ -1,4 +1,15 @@
 $(document).ready(function(){
+	// Change from style positioning to custom data attributes
+	// $('.map-point').each(function(){
+	// 	var top = $(this).attr('data-top');
+	// 	var left = $(this).attr('data-left');
+	// 	$(this).animate({
+	// 		top: top + 'px',
+	// 		left: left + 'px'
+	// 	});
+	// });
+
+
   var activeClass = 'active';
 
   // $('.centered-y > p > a').click(function(e){
@@ -12,7 +23,7 @@ $(document).ready(function(){
     $(this).addClass(activeClass);
     e.stopPropagation();
 		var useThis = $(this);
-		
+
 
   });
 
