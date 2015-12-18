e4b55d317a5b4191e5472a0bc7cf2244a3a16cfd
Celso Mireles <celso@digitalstrategy.tips>
2015-12-17 18:19:33 -0700

    Compiled, add gitignore

--- www/js/index.js
+++ www/js/index.js
@@ -1,21 +1,30 @@
 $(document).ready(function(){
+	// Change from style positioning to custom data attributes
+	$('.map-point').each(function(){
+		var top = $(this).attr('data-top') + '%';
+		var left = $(this).attr('data-left') + '%';
+		$(this).animate({
+			top: top,
+			left: left
+		});
+	});
+
+
   var activeClass = 'active';
 
-  $('.centered-y > p > a, a').click(function(e){
-    e.preventDefault();
-    var url = $(this).attr('href');
-    console.log(url + ' clicked');
-    window.open(url, '_blank');
-  });
 
   $('.map-point').click(function(e){
     $('.map-point').removeClass(activeClass);
     $(this).addClass(activeClass);
     e.stopPropagation();
+		var useThis = $(this);
+
+
   });
 
   $('body').click(function(e){
     $('.map-point').removeClass(activeClass);
     e.stopPropagation();
+
   });
 });
